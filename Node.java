e55df454049fd09b3530013a9a29613f4563ac68
keilo <keilo.at@gmail.com>
2010-11-23 13:14:58 +0000

    added node data structure, need to merge this with strawberryutopia's code

---
+++ Node.java
@@ -0,0 +1,20 @@
+
+// Tree node
+
+public class Node {
+
+	Node parent;
+	
+	public setParent(Node newParent)
+	{
+		this.parent = newParent;
+	}
+	
+	Node public getParent()
+	{
+		return this.parent;
+	}
+	
+	
+	
+}
